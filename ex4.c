dcbe36bf8dd4181400ce644b80dc23242bbcd8a8
Kiyoshhiii <115041179+Kiyoshhiii@users.noreply.github.com>
2023-03-14 15:44:35 -0300

    Add files via upload

---
+++ ex4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    //S = S0 + v*t;
+    //SA = SB
+    //110*t = 100 - 80*t + 1.7*t
+    //eles secruzam em aproximadamente 35 min;
+    //ou seja o carro ja andou aproximadamente 70km em meia hora, enquanto o caminhao andou aproximadamente 30 km
+    //porem como queremos saber a distancia com relação a ribeirao preto no momento em que se cruzam
+    //a distancia permanece a mesma, ja que os dois estao no mesmo ponto
+}
